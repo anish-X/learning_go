86117a936204f44ed2b4ad3ba629112f3d715043
anish-X <anish.stha527@gmail.com>
2025-12-16 12:29:35 +0545

    hello, world

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main // way to group the function on same package called main 
+
+import "fmt" // formatting and priniting to console; standard lib
+
+func main() { // main func executes by default when running the main packages
+	fmt.Println("hello, world!");
+}
+
+
