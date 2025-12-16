7a3ef3bca0e5937dec4cbd307a786c0bf3ba13e0
anish-X <anish.stha527@gmail.com>
2025-12-16 16:58:17 +0545

    feat: comment

--- day1/hello/hello.go
+++ day1/hello/hello.go
@@ -8,13 +8,14 @@ import (
 
 func main() {
 
-	log.SetPrefix("greetings: ")
+	log.SetPrefix("greetings: ") // greetings prefix is added to every log message
 	log.SetFlags(0)
 
 	message, err := greetings.Hello("")
 
 	if err != nil {
-		log.Fatal(err) // error information
+		log.Println("check") // --> greetings: check
+		log.Fatal(err)       // error information
 	}
 
 	fmt.Println(message, err)
